681866d46f6a769dbb8737b4b1cc87eb93eb9cd0
DDPS course <ddps2110@fs0.cm.cluster>
2021-11-02 17:13:25 +0100

    improved config

--- configure.sh
+++ configure.sh
@@ -4,11 +4,24 @@ HADOOP_HOME=${HOME}/scratch/hadoop-3.3.0
 ALLUXIO_HOME=${HOME}/scratch/alluxio-2.6.2
 SPARK_HOME=${HOME}/scratch/spark-3.2.0-bin-hadoop3.2
 NODE_IP_PREFIX=10.149
-N_SPECIAL_NODES=1
+N_SPECIAL_NODES=3
 
 RESERVE_N=5
 RESERVE_T=00:15:00
 
+
+### Read script arguments
+while getopts ":n:" opt; do
+	case $opt in
+		n)
+			if [ $OPTARG -lt $N_SPECIAL_NODES ]; then
+				echo: "ERROR: Amount of reserved nodes must >= N_SPECIAL_NODES, exiting..."
+				exit_abnormal
+			fi
+			RESERVE_N=$OPTARG
+		;;
+	esac
+done
 
 ### Reserve required amount of nodes
 module load prun
@@ -36,8 +49,8 @@ echo "nodeIPList: ${nodeIPList[@]} (done)"
 
 ### Determine IP addresses of special nodes
 nameNodeIP=${nodeIPList[0]}
-alluxioMasterIP=${nodeIPList[0]}
-sparkMasterIP=${nodeIPList[0]}
+alluxioMasterIP=${nodeIPList[1]}
+sparkMasterIP=${nodeIPList[2]}
 
 
 ### Set up Hadoop config files(sed command is some real magic)
@@ -82,10 +95,17 @@ done
 
 ### Execute startup scripts
 #Execute Hadoop (HDFS+YARN) startup script on namenode
-ssh ddps2110@${nameNodeIP} "~/startHDFS.sh -f"
+#ssh ddps2110@${nameNodeIP} "~/startHDFS.sh -f"
 
 #Execute Alluxio startup script on Alluxio master
-ssh ddps2110@${alluxioMasterIP} "~/startAlluxio.sh"
+#ssh ddps2110@${alluxioMasterIP} "~/startAlluxio.sh"
 
 # Execute Spark startup script on spark master
-ssh ddps2110@${sparkMasterIP} "~/startSpark.sh"
+#ssh ddps2110@${sparkMasterIP} "~/startSpark.sh"
+
+
+### Echo information
+echo "Successfully initialized cluster consisting of ${RESERVE_N} nodes"
+echo "HDFS NameNode: ${nameNodeIP}"
+echo "Alluxio master: ${alluxioMasterIP}"
+echo "Spark master: ${sparkMasterIP}"
