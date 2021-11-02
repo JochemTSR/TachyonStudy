ce0d0bca2046d55d127ce9ebd385dcafd0f1c01e
Patrick Star (J.T.S. Ram) <j.t.s.ram@umail.leidenuniv.nl>
2021-11-02 17:30:12 +0100

    split benchmark file

---
+++ benchHDFS.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Script that benchmarks filesystem performance of memHDFS compared to Alluxio.
+# Should be run from namenode of running HDFS cluster
+
+HADOOP_HOME=~/scratch/hadoop-3.3.0
+HADOOP_PORT=8020
+ALLUXIO_PORT=19998
+N_FILES=64
+WRITE_SIZE=1GB
+HDFS_OUT_FILE=~/dfsio_hfds.txt
+
+
+### Stop HDFS and YARN daemons if running
+./stopHDFS.sh
+
+
+### Edit config file to use HDFS filesystem
+sed -i "s/<value>alluxio:\/\//<value>hdfs:\/\//" ${HADOOP_HOME}/etc/hadoop/core-site.xml
+sed -i "s/${ALLUXIO_PORT}/${HADOOP_PORT}/" ${HADOOP_HOME}/etc/hadoop/core-site.xml
+
+
+### Start HDFS under Ramdisk and YARN daemons
+./startHDFS.sh -f -r
+
+
+### Run benchmark from all workers
+${HADOOP_HOME}/bin/hadoop jar ${HADOOP_HOME}/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-3.3.0-tests.jar TestDFSIO -write -nrFiles $N_FILES -fileSize $WRITE_SIZE -resfile $HDFS_OUT_FILE
